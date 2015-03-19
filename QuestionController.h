a3455736659bb03048e1237e21e5aa3c10c4323e
DavidMonson <davidcmonson@gmail.com>
2015-03-19 10:46:05 -0600

    Adds Core Data Stack and methods in Question Controller

--- QuestionController.h
+++ QuestionController.h
@@ -7,7 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Question.h"
+#import "User.h"
+
 
 @interface QuestionController : NSObject
+
+@property (nonatomic, strong, readonly) NSArray *questions;
+@property (nonatomic, strong, readonly) User *currentUser;
+
+
+- (Question *)createQuestionWithTitle:(NSString *)title details:(NSString *)details;
+
+- (void)removeQuestion:(Question *)question;
+
+- (void)save;
+
+typedef NS_ENUM(NSUInteger, QuestionStatus) {
+    StatusActive,
+    StatusAnswered,
+    StatusDeleted,
+};
+
+typedef NS_ENUM(NSUInteger, UserRole) {
+    RoleStudent,
+    RoleMentor,
+};
+
+
 
 @end
--- QuestionController.m
+++ QuestionController.m
@@ -7,7 +7,64 @@
 //
 
 #import "QuestionController.h"
+#import "Stack.h"
+
 
 @implementation QuestionController
+
+- (NSArray *)questions {
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Question"];
+    
+    return [[Stack sharedInstance].managedObjectContext executeFetchRequest:request error:nil];
+    
+}
+
+- (User *)currentUser {
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"User"];
+    
+    NSArray *users = [[Stack sharedInstance].managedObjectContext executeFetchRequest:request error:nil];
+    
+    User *user;
+    
+    if (users.firstObject) {
+        user = users.firstObject;
+    } else {
+        user = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+        
+        user.name = @"Current User";
+        user.role = [NSNumber numberWithInteger:RoleStudent];
+    }
+    
+    return users.firstObject;
+    
+}
+
+- (Question *)createQuestionWithTitle:(NSString *)title details:(NSString *)details {
+    Question *question = [NSEntityDescription insertNewObjectForEntityForName:@"Entry" inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    question.title = title;
+    question.details = details;
+    question.dateSubmitted = [NSDate date];
+    question.status = [NSNumber numberWithInteger:StatusActive];
+    question.submittedBy = self.currentUser;
+    
+    [self save];
+    
+    return question;
+}
+
+- (void)removeQuestion:(Question *)question {
+    [question.managedObjectContext deleteObject:question];
+    [self save];
+    
+}
+
+- (void)save {
+    [[Stack sharedInstance].managedObjectContext save:nil];
+    
+}
+
+
+
 
 @end
